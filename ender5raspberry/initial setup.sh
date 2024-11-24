3cb7ca84be52d225c84424a101b780585c2ad637
edmondlafay <edmond.lafay@jobteaser.com>
2024-11-24 12:28:04 +0100

    add KAMP to ender5

--- ender5raspberry/initial setup.sh
+++ ender5raspberry/initial setup.sh
@@ -1,3 +1,5 @@
+cd ~
+
 sudo apt-get update && sudo apt upgrade -y
 
 # install cockpit for easy administration
@@ -7,7 +9,12 @@ sudo apt install cockpit -y
 sudo apt-get install git -y
 
 # install kiauh to help installing klipper
-cd ~ && git clone https://github.com/dw-0/kiauh.git
+git clone https://github.com/dw-0/kiauh.git
 
 # launch kiauh config menu
 ./kiauh/kiauh.sh
+
+# install KAMP
+git clone https://github.com/kyleisah/Klipper-Adaptive-Meshing-Purging.git
+ln -s ~/Klipper-Adaptive-Meshing-Purging/Configuration printer_data/config/KAMP
+cp ~/Klipper-Adaptive-Meshing-Purging/Configuration/KAMP_Settings.cfg ~/printer_data/conf
